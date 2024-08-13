6a904c5f1e5bc99e601e80353d89a9547c2b2f96
HarshVardhan Rajaiah <rharshvardhan9435@gmail.com>
2024-08-13 09:01:01 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from 'react'
+import Navbar from './components/Navbar';
 function App() {
+const [mode, setmode] = useState("light")
+const [btn, setbtn] = useState("Dark Mode Disabled")
+const toggle =()=>{
+  if(mode == "light"){
+    setmode("dark")
+    setbtn("Dark Mode Enabled")
+    document.body.style.backgroundColor='black'
+   }
+  else {
+    setmode("light")
+    setbtn("Dark Mode Disabled")
+    document.body.style.backgroundColor='white'
+  }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <>
+     <Navbar mode={mode}   toggle={toggle} />
+     </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function navbar(props) {
+  return (
+    <div>
+<nav className="navbar navbar-expand-lg" style={{backgroundColor:props.mode==='light' ?'white':'black',color:props.mode==='light'?'black':'white'}}>
+  <div className="container-fluid" >
+    <a style={{backgroundColor:props.mode==='light' ?'white':'black',color:props.mode==='light'?'black':'white'}} className="navbar-brand" href="/">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a  style={{backgroundColor:props.mode==='light' ?'white':'black',color:props.mode==='light'?'black':'white'}}className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a style={{backgroundColor:props.mode==='light' ?'white':'black',color:props.mode==='light'?'black':'white'}} className="nav-link" href="/">Link</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div className="form-check form-switch">
+  <input onClick={props.toggle} className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode}</label>
+</div>
+</nav>
+    </div>
+  )
+}
